bf515433e999efccbd4bb33de6ac3ea6306059dd
Sam <32462530+samlowe106@users.noreply.github.com>
2020-05-05 12:16:59 -0400

    Removed download_file and get_url_extension. Renamed download_img_from to download_image and refactored.

--- app/urlhelpers.py
+++ app/urlhelpers.py
@@ -5,10 +5,66 @@ import main
 import os
 import requests
 import shutil
-from typing import List, Tuple
+from typing import List
+from requests.models import Response
 
 
-def find_urls(url: str) -> List[str]:
+def download_image(url: str, title: str, directory: str, png: bool = False, temp_dir: str = "temp") -> bool:
+    """
+    Downloads the linked image, converts it to the specified filetype,
+    and saves to the specified directory. Avoids name conflicts.
+    :param url: url directly linking to the image to download
+    :param title: title that the final file should have
+    :param dir: directory that the final file should be saved to
+    :return: True if the file was downloaded correctly, else False 
+    """
+
+    # Save the image to a temp directory
+    r = requests.get(url)
+    
+    if r.status_code != 200:
+        return False
+
+    extension = get_extension(r)
+
+    temp_filepath = os.path.join(temp_dir, title + extension)
+
+    write_file(r, temp_filepath)
+
+    # Convert to png if necessary
+    if png:
+        extension = ".png"
+        app.filehelpers.convert(temp_filepath, extension)
+        # Reassign temp_filepath with new extension
+        temp_filepath = os.path.join(temp_dir, title + extension)
+
+    # Move to desired directory
+    final_filename = app.filehelpers.prevent_collisions(title, extension, directory)
+    final_filepath = os.path.join(directory, final_filename)
+    shutil.move(temp_filepath, final_filepath)
+    
+    # Remove temp file
+    os.remove(temp_filepath)
+
+    return True
+
+
+def get_extension(r: Response) -> str:
+
+    return r.headers["Content-type"].split("/")[1].lower()
+
+
+def write_file(r: Response, filepath: str) -> bool:
+    """
+    """
+    # Write the file
+    with open(filepath, "wb") as f:
+        f.write(r.content)
+
+    return True
+
+
+def find_urls(r: Response) -> List[str]:
     """
     Attempts to find images on a linked page
     Currently supports directly linked images and imgur pages
@@ -16,57 +72,44 @@ def find_urls(url: str) -> List[str]:
     :return: a list of direct links to images found on that webpage
     """
 
-    extension = app.urlhelpers.get_url_extension(url)
+    extension = get_extension(r)
 
     # If the image in the url has a recognized file extension, this is a direct link to an image
     # (Should match artstation, i.imgur.com, i.redd.it, and other direct pages)
-    if is_recognized(extension):
-        return [url]
+    if recognized(extension):
+        return [r.url]
 
     # Imgur
-    elif "imgur.com" in url and not url.endswith("/gallery/"):
+    elif "imgur.com" in r.url and not r.url.endswith("/gallery/"):
         # Albums    
-        if "/a/" in url:
-            return parse_imgur_album(url)
+        if "/a/" in r.url:
+            return parse_imgur_album(r.url)
 
         # Galleries (might be albums or singles)
-        elif "/gallery/" in url:
-            data = requests.get(url + ".json")
+        elif "/gallery/" in r.url:
+            data = requests.get(r.url + ".json")
             gallery_dict = json.loads(data.content)
             if gallery_dict["data"]["image"]["is_album"]:
-                return parse_imgur_album(url)
+                return parse_imgur_album(r.url)
             else:
-                return [parse_imgur_single(url)]
+                return [parse_imgur_single(r.url)]
 
         # Single-image page
         else:
-            return [parse_imgur_single(url)]
+            return [parse_imgur_single(r.url)]
 
     # Unrecognized pages
     else:
         return []
 
 
-def is_recognized(extension: str, recognized: List[str] = [".png", ".jpg", ".jpeg", ".gif"]) -> bool:
+def recognized(extension: str) -> bool:
     """
     Checks if the specified extension is recognized
     :param extension: the extension to check
     :return: True if the extension is recognized, False otherwise
     """
-    return extension.lower() in recognized
-    
-
-def get_url_extension(url: str) -> str:
-    """
-    Gets the extension from the given url
-    :param url: a direct link to an image
-    :return: the file extension of the linked page
-    """
-    # https://stackoverflow.com/a/32651400
-    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
-    r = requests.get(url, {'Content-type': 'content_type_value'})
-    mime_type = r.headers["Content-type"]
-    return "." + mime_type.split("/")[1]
+    return extension in [".png", ".jpg", ".jpeg", ".gif"]
 
 
 def parse_imgur_album(album_url: str) -> List[str]:
@@ -92,63 +135,3 @@ def parse_imgur_single(url: str) -> str:
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
     return soup.select("link[rel=image_src]")[0]["href"]
-
-
-def download_img_from(url: str, title: str, dir: str, png: bool = False, temp: str = "temp") -> bool:
-    """
-    Downloads the linked image, converts it to the specified filetype,
-    and saves to the specified directory. Avoids name conflicts.
-    :param url: url directly linking to the image to download
-    :param title: title that the final file should have
-    :param dir: directory that the final file should be saved to
-    :return: True if the file was downloaded correctly, else False 
-    """
-
-    # Save the image to a temp directory
-    try:
-        download_file(title, url, temp)
-    except ConnectionError:
-        return False
-
-    # Convert to png if necessary
-    if png:
-        app.filehelpers.convert_file(temp, ".png")
-        extension = ".png"
-        temp_path = os.path.join(temp, title + extension)
-
-    # Move to desired directory
-    final_filename = app.filehelpers.prevent_conflicts(title, extension, dir)
-    final_filepath = os.path.join(dir, final_filename)
-    shutil.move(temp_path, final_filepath)
-
-    return True
-
-
-def download_file(url: str, directory: str, title: str) -> None:
-    """
-    Saves the file on the linked page to the specified path with the specified title and file extension
-    :param url: a direct link to the page to be saved
-    :param directory: the path the page should be saved to
-    :param title: the title the page file should have (sans extension)
-    :return: None
-    """
-
-    r = requests.get(url, {'Content-type': 'content_type_value'})
-    
-    # The page page couldn't be reached
-    if r.status_code != 200:
-        return ConnectionError('Request failed with error {0}.'.format(r.status_code))
-
-    content_type = r.headers["Content-type"]
-    extension = "." + content_type[(content_type.find("/") + 1):]
-
-    # Output path
-    app.filehelpers.create_directory(directory)
-
-    filepath = os.path.join(directory, title + extension)
-
-    # Write the file
-    with open(filepath, "wb") as f:
-        f.write(r.content)
-
-    return

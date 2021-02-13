163a169f6744f3fbb09f6c78267177750c16d22e
Sam Lowe <samlowe106@gmail.com>
2021-02-13 17:33:06 -0500

    Refactored login process

--- src/main.py
+++ src/main.py
@@ -17,16 +17,16 @@ URLTuple = Tuple[str, bool]
 temp_dir = "temp"
 log_directory = "Logs"
 log_path = os.path.join("Logs", "log.txt")
+info_path = "info.txt"
 
 
 def main() -> None:
     """
     Scrapes and downloads any images from posts in the user's saved posts category on Reddit
     """
-    read_client_info()
 
-    if (reddit := prompt_sign_in()) is None:
-        print("Username and password unrecognized.")
+    if (reddit := sign_in(info_path)) is None:
+        print("Unrecognized username or password.")
         return
 
     files.create_directory(temp_dir)
@@ -68,36 +68,39 @@ def cleanup():
     """
 
 
-def prompt_sign_in() -> Optional[Reddit]:
+def sign_in(filepath: str) -> Optional[Reddit]:
     """
-    Prompts the user to sign in
-    :return: Reddit object
+    Attempts to sign into Reddit
+    :param filepath: Path to the text file containing the client ID and client secret
+    :return: reddit object if successful, else None
     """
+    try:
+        with open(filepath, 'r') as info_file:
+            client_id = info_file.readline()
+            client_secret = info_file.readline()
+    except FileNotFoundError:
+        print(f"The client info file couldn't be found at {filepath}")
+        return
+    
+    # praw returns an invalid reddit instance if the  client id or client secret are ""
+    if not client_id:
+        print("Client ID is blank!")
+        return
+    if not client_secret:
+        print("Client Secret is blank!")
+        return
+
     # getpass only works through the command line!
     if (username := input("Username: ")) and (password := getpass.getpass("Password: ")):
         print("Signing in...", end="")
-        return sign_in(username, password)
-
-
-def sign_in(username: str, password: str) -> Optional[Reddit]:
-    """
-    Attempts to sign into Reddit
-    :param username: The user's username
-    :param password: The user's password
-    :return: reddit object if successful, else None
-    :raises ConnectionException: if username and password were unrecognized
-    """
-    client_id, client_secret = read_client_info()
-
-    try:
-        return praw.Reddit(client_id=client_id,
-                           client_secret=client_secret,
-                           user_agent='PaperScraper',
-                           username=username,
-                           password=password)
-    # Catch and re-raise error with a more helpful message
-    except OAuthException:
-        return None
+        try:
+            return praw.Reddit(client_id=client_id,
+                            client_secret=client_secret,
+                            user_agent='PaperScraper',
+                            username=username,
+                            password=password)
+        except OAuthException:
+            return
 
 
 def read_client_info(filepath: str = "info.txt") -> Tuple[str, str]:
@@ -105,15 +108,10 @@ def read_client_info(filepath: str = "info.txt") -> Tuple[str, str]:
     Reads the client ID and client secret from the specified file
     :param filepath: filepath of the file to read the client id and secret from
     :return: tuple containing the client ID and secret in that order
+    :raises: FileNotFoundError, ValueError
     """
-    assert os.path.isfile(filepath), filepath + " couldn't be found!"
-    with open(filepath, 'r') as info_file:
-        client_id = info_file.readline()
-        client_secret = info_file.readline()
-    # praw returns an invalid reddit instance if the  client id or client secret are ""
-    assert client_id, "Client ID is blank!"
-    assert client_secret, "Client Secret is blank!"
-    return (client_id, client_secret)
+
+    
 
 
 if __name__ == "__main__":

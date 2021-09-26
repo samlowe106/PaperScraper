439b86b7e553390f39f7e0d9584e5b7ac0e2893d
Samuel Lowe <samlowe106@gmail.com>
2021-09-26 15:39:38 -0400

    Added FlaskWebView

--- src/view/view.py
+++ src/view/view.py
@@ -8,16 +8,30 @@ class View():
     """
 
 
-    def output(self, message: str):
-        pass
+    def output(self, message: str) -> None:
+        """
+        Shows output to the user
+        :param message: message to show the user
+        """
 
 
-    def get_input(self, prompt: str) -> str:
-        pass
+    def get_input(self, prompt: str, field: str) -> str:
+        """
+        Gets user input
+        NOTE: DO NOT USE FOR SENSITIVE DATA! For sensitive data such as passwords,
+        use get_password instead!
+        :param prompt: prompt to show the user
+        :param field: the field from which to retrieve user input (if necessary)
+        :return: user input
+        """
 
 
     def get_password(self, prompt: str) -> str:
-        pass
+        """
+        Gets sensitive user input, such as a password, in a secure way
+        :param prompt: prompt to show the user
+        :return: user input
+        """
 
 
 class TextView(View):
@@ -25,14 +39,67 @@ class TextView(View):
     Class representing a way to interact with PaperScraper through the Terminal or Command Line
     """
 
+    def __init__(self):
+        return
+
 
     def print_message(self, message: Any):
+        """
+        Shows output to the user
+        :param message: message to show the user
+        """
         print(message)
 
 
-    def get_input(self, prompt: str) -> str:
+    def get_input(self, prompt: str, field = None) -> str:
+        """
+        Gets user input
+        NOTE: DO NOT USE FOR SENSITIVE DATA! For sensitive data such as passwords,
+        use get_password instead!
+        :param prompt: prompt to show the user
+        :param field: (not used)
+        :return: user input
+        :raise ValueError:
+        """
+        if field is not None:
+            raise ValueError("No value should be passed for field!")
         return input(prompt)
 
 
     def get_password(self, prompt: str) -> str:
         return getpass(prompt)
+
+
+class FlaskWebView(View):
+    """
+    Class representing a way to interact with PaperScraper through a Flask website
+    """
+
+    def print_message(self, message: Any):
+        """
+        Shows output to the user
+        :param message: message to show the user
+        """
+        #TODO
+        str(message)
+
+
+    def get_input(self, prompt: str, field: str) -> str:
+        """
+        Gets user input
+        NOTE: DO NOT USE FOR SENSITIVE DATA! For sensitive data such as passwords,
+        use get_password instead!
+        :param prompt: prompt to show the user
+        :param field: the field from which to retrieve user input
+        :return: user input
+        """
+        #TODO
+
+
+    def get_password(self, prompt: str) -> str:
+        """
+        Gets sensitive user input, such as a password, in a secure way
+        :param prompt: prompt to show the user
+        :return: user input
+        """
+        #TODO

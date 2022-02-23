26a1ffbe4084c4bcfdada28855315fc529ecc30e
Samuel Lowe <samlowe106@gmail.com>
2022-02-23 02:16:32 -0500

    Added imports and docstrings

--- src/view/view.py
+++ src/view/view.py
@@ -1,8 +1,7 @@
 from getpass import getpass
-from sys import argv
-from typing import Dict
-from PaperScraper.src.model.submission_wrapper import SubmissionWrapper
-from praw import Reddit
+from typing import Any, Dict
+from flask import Flask, render_template, request, send_file
+from ..model import SubmissionWrapper
 
 
 class View():
@@ -10,13 +9,19 @@ class View():
     Interface representing a way to interact with PaperScraper
     """
 
-    def get_run_info(self) -> Dict[str, str]:
+    def get_run_info(self, args : Dict[str, Any]) -> Dict[str, str]:
+        """
+        Gets parameters about this run
         """
 
+    def show_results(self, output_directory: str):
+        """
+        Displays information about the downloaded files at the end of the run
         """
 
     def output_submission_wrapper(self, index: int, post: SubmissionWrapper):
         """
+        Outputs a submission wrapper so the user can see it
         """
 
     def print_message(self, message: str):
@@ -25,7 +30,7 @@ class View():
         :param message: message to show the user
         """
 
-class TextView(View):
+class TerminalView(View):
     """
     Class representing a way to interact with PaperScraper through the Terminal or Command Line
     """
@@ -36,9 +41,10 @@ class TextView(View):
 
     def get_run_info(self, args) -> Dict[str, str]:
         """
-
+        Gets parameters about this run
         """
         args["password"] = getpass("Password: ")
+
         return args
 
 
@@ -48,3 +54,66 @@ class TextView(View):
         :param message: message to show the user
         """
         print(message)
+
+
+class FlaskWebView():
+    """
+    Interface representing a way to interact with PaperScraper
+    """
+
+    app = Flask(__name__)
+
+    def __init__(self) -> None:
+        self.submission_wrappers = []
+        self.app.run()
+
+
+    @app.route('/', methods=["POST", "GET"])
+    def index(self):
+        """
+        Landing page for PaperScraper, takes user input and run parameters
+        """
+
+
+    @app.route('/download')
+    def download_file(self):
+        """
+        Pushes the zip file to the user
+        """
+        path = ""
+        return send_file(path, as_attachment=True)
+
+
+    @app.route('/', methods=["POST", "GET"])
+    def result(self):
+        """
+        Results page, after user has told PaperScraper to start running
+        """
+
+    def output(self, message: str, error: bool = False) -> None:
+        """
+        Shows output to the user
+        :param message: message to show the user
+        :param error: whether the message is an error message or not
+        """
+
+        return render_template('index.html')
+
+
+    def get_run_info(self, _) -> Dict[str, any]:
+        """
+        Gets parameters about this run
+        """
+        return {"username" : request.form("username"),
+                "password" : request.form("password"),
+                "limit" : int(request.form("limit")),
+                "title" : request.form("title"),
+                "organize" : request.form("organize"),
+                "directory" : ""} # TBD
+
+
+    def output_submission_wrapper(self, _, wrapper: SubmissionWrapper):
+        """
+        Outputs the submission wrapper to the user
+        """
+        self.submission_wrappers.append(wrapper)

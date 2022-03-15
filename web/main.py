95be9290a7d17d168d3d532edfb5ae17e98ccd85
Samuel Lowe <samlowe106@gmail.com>
2022-03-14 21:18:23 -0400

    small text changes

--- web/main.py
+++ web/main.py
@@ -1,68 +1,79 @@
-from typing import Dict, Any
+import argparse
 from shutil import make_archive
-from flask import Flask, send_file, render_template
-from ..core.models import SubmissionWrapper
-
+from typing import Dict
+from flask import Flask, request, send_file, render_template
+from prawcore.exceptions import OAuthException
+from PaperScraper.core import sign_in
+from PaperScraper.core.models import SubmissionSource
 
 app = Flask(__name__)
 
-def __init__(self) -> None:
-    self.submission_wrappers = []
-    self.output_path = None
-    self.app.run()
 
-
-def show_results(self, output_directory: str):
-    """
-    Displays information about the downloaded files at the end of the run
-    """
-    self.output_path = make_archive("paperscraper_output", "zip", output_directory)
+OUTPUT_DIRECTORY = "TODO"
 
 
 @app.route('/', methods=["POST", "GET"])
-def root(self):
+def root():
     """
     Landing page for PaperScraper, takes user input and run parameters
     """
+    if request.method == "GET":
+        return render_template("root.html")
+
+    elif request.method == "POST":
+        error = None
+        source = None
+        try:
+            source = get_source(request.form)
+        except OAuthException:
+            error = "Invalid username or password."
+        except ValueError as error:
+            error = str(error)
+
+        return render_template("root.html", error=error, source=source)
+
+
+def get_source(form: Dict[str, str]) -> SubmissionSource:
+    """
+    Gets the source from which posts will be parsed and displayed
+    """
+    username = form["username"]
+    password = form["password"]
+    subreddit = form["subreddit"]
+
+    if subreddit and username and password:
+        raise ValueError("Please specify only a subreddit or a username and password.")
+    if not (subreddit and username and password):
+        raise ValueError("Please specify a subreddit or a username and password.")
+
+    source_builder = (SubmissionSource.Builder()
+        .age_max(form["age"])
+        .submission_limit(form["limit"])
+        .score_min(form["karma"]))
+
+    if username and password:
+        return source_builder.from_user_saved(sign_in(username, password)).build()
+
+    elif subreddit:
+        return source_builder.from_subreddit(subreddit, form["sortby"], sign_in()).build()
 
 
 @app.route('/download')
-def download_file(self):
+def download_file():
     """
     Pushes the zip file to the user
     """
-    return send_file(self.output_path, as_attachment=True)
+    return send_file(
+        make_archive("paperscraper_output", "zip", OUTPUT_DIRECTORY),
+        as_attachment=True)
 
 
-@app.route('/', methods=["POST", "GET"])
-def result(self):
-    """
-    Results page, after user has told PaperScraper to start running
-    """
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            description="Scrapes images from Reddit")
 
-def output(self, message: str, error: bool = False) -> None:
-    """
-    Shows output to the user
-    :param message: message to show the user
-    :param error: whether the message is an error message or not
-    """
-    return render_template('index.html')
-
-
-def get_run_info(self, request) -> Dict[str, Any]:
-    """
-    Gets parameters about this run
-    """
-    return {"username" : request.form("username"),
-            "password" : request.form("password"),
-            "limit" : int(request.form("limit")),
-            "title" : request.form("title"),
-            "organize" : request.form("organize"),
-            "directory" : ""} # TBD
-
-
-def output_submission_wrapper(self, _, wrapper: SubmissionWrapper):
-    """
-    Outputs the submission wrapper to the user
-    """
-    self.submission_wrappers.append(wrapper)
+    parser.add_argument("--debug",
+                        action='store_true',
+                        help="run in debug mode")
+    args = parser.parse_args()
+    app.run(debug=args.debug)

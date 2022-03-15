958df503cc9713ed75949888af81b35a98ec968f
Samuel Lowe <samlowe106@gmail.com>
2022-03-14 21:15:26 -0400

    Fixed imports

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,5 +1,5 @@
-from .parsers import *
+from .parsers import IParser, ImgurParser, SingleImageParser, FlickrParser, GfycatParser, PARSERS
 from ..utils.strings import *
-from .submission_wrapper import *
+from .submission_wrapper import SubmissionWrapper
 from ..utils.urls import *
-from .submission_source import *
+from .submission_source import SubmissionSource, SortOption

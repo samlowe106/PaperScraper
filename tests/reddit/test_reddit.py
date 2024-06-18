9c3ae7b8826f9a6ca619365057ac32dbdfcfe30d
Samuel Lowe <samlowe106@gmail.com>
2024-06-17 23:04:45 -0400

    Added more tests

--- tests/reddit/test_reddit.py
+++ tests/reddit/test_reddit.py
@@ -1,8 +1,11 @@
+import datetime
 import os
 import unittest
-from unittest.mock import patch
+import uuid
+from unittest.mock import Mock, patch
 
 import core
+from core import SubmissionWrapper
 from tests import SubmissionWrapperFactory
 
 
@@ -53,9 +56,38 @@ class TestSignIn(unittest.TestCase):
 
 class TestSubmissionWrapper(unittest.TestCase):
     def test_constructor(self):
-        # TODO
-        # wrapper = SubmissionWrapperFactory()
-        pass
+        submission_mock = Mock()
+        submission_mock.title = "mock ?? title: \\"
+        submission_mock.subreddit = uuid.uuid4()
+        submission_mock.url = str(uuid.uuid4())
+        submission_mock.author = str(uuid.uuid4())
+        submission_mock.over_18 = False
+        submission_mock.score = 10
+        submission_mock.created_utc = datetime.utcnow()
+        submission_mock.saved = Mock()
+
+        client_mock = Mock()
+
+        dry = True
+
+        wrapper = SubmissionWrapper(submission_mock, client_mock, dry=dry)
+
+        # Assert that the wrapper has the correct values
+        self.assertEqual(wrapper.title, submission_mock.title)
+        self.assertEqual(wrapper.subreddit, submission_mock.subreddit)
+        self.assertEqual(wrapper.url, submission_mock.url)
+        self.assertEqual(wrapper.author, submission_mock.author)
+        self.assertEqual(wrapper.nsfw, submission_mock.over_18)
+        self.assertEqual(wrapper.score, submission_mock.score)
+        self.assertEqual(wrapper.created_utc, submission_mock.created_utc)
+        self.assertEqual(wrapper.dry, not dry)
+        # Assert that the file title is uppercased and has the special characters removed
+        self.assertEqual(wrapper.file_title, "Mock Title")
+        # Assert values like responses and urls are not set
+        self.assertIsNone(wrapper.response)
+        self.assertIsNone(wrapper.urls)
+        # Assert that the submission is has not been unsaved
+        self.submission_mock.unsave.assert_not_called()
 
     def test_unsave(self):
         # Assert SubmissionWrappers do not unsave posts when dry = True
@@ -78,9 +110,18 @@ class TestSubmissionWrapper(unittest.TestCase):
         self.assertTrue(wrapper.unsave(force=True))
         wrapper._submission.unsave.assert_called_once()
 
+    def test_download_all_empty(self):
+        # Assert that an empty list of urls returns an empty dictionary
+        wrapper = SubmissionWrapperFactory()
+        self.assertEqual(wrapper.download_all(), dict())
+
+    @patch("IO.write")
+    @patch("httpx.Response")
+    @patch("os.makedirs")
     def test_download_all(self):
+        wrapper = SubmissionWrapperFactory()
+        wrapper.urls = ["url1", "url2", "url3", "url4"]
         # TODO
-        pass
 
     @patch("open")
     @patch("json.dump")

016892fd3962eb7ee8509f1c207aa07fe02cf3ca
Samuel Lowe <samlowe106@gmail.com>
2024-12-25 00:07:01 -0500

    Removed functions not used outside of core

--- core/__init__.py
+++ core/__init__.py
@@ -4,9 +4,7 @@ from .core import (
     get_extension,
     remove_invalid,
     retitle,
-    sandbox,
     shorten,
     title_case,
-    trim_string,
 )
 from .reddit import SortOption, SubmissionWrapper, from_saved, from_subreddit, sign_in

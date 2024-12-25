aeb221a8bbe0c674ca54ec8aee87ebf346ab9363
Samuel Lowe <samlowe106@gmail.com>
2024-12-25 10:20:28 -0500

    Core tests now use vcr to prevent actual network calls

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,16 +1,29 @@
-from core import file_title
+import httpx
+import pytest
+
+from core import get_extension, retitle  # determine_name,
 
 
-def test_remove_invalid():
-    pass
+class TestGetExtension:
+    """Actually makes get requests!"""
+
+    @pytest.mark.vcr
+    def test_jpeg(self):
+        # Mushroom Generator by beeple
+        r = httpx.get(
+            "https://cdna.artstation.com/p/assets/images/images/012/127/414/large/beeple-07-25-18.jpg?1533161904"
+        )
+        assert get_extension(r) == ".jpeg"
+
+    @pytest.mark.vcr
+    def test_png(self):
+        # Early Summer Holiday by 六七質
+        r = httpx.get("https://i.redd.it/1u3xx7t7tmra1.png")
+        assert get_extension(r) == ".png"
 
 
-def test_reduce_whitespace():
-    pass
-
-
-def test_file_title():
-    assert file_title("mock title") == "mock title"
-    assert file_title("mock title: \\") == "mock title"
-    assert file_title("mock title: ??") == "mock title"
-    assert file_title("mock ?? title: \\") == "mock title"
+def test_retitle():
+    assert retitle("mock title") == "mock title"
+    assert retitle("mock title: \\") == "mock title"
+    assert retitle("mock title: ??") == "mock title"
+    assert retitle("mock ?? title: \\") == "mock title"

03fcb00b9d040099c997ce64ab37aadc64dba8b4
Samuel Lowe <samlowe106@gmail.com>
2024-12-25 01:14:23 -0500

    Removed functions that weren't being used

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,2 @@
-from .core import (
-    determine_name,
-    file_title,
-    get_extension,
-    remove_invalid,
-    retitle,
-    shorten,
-    title_case,
-)
+from .core import determine_name, file_title, get_extension
 from .reddit import SortOption, SubmissionWrapper, from_saved, from_subreddit, sign_in

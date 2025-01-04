b4b60ce7e7f88bf3e5e5fb9c9be864af2e89eb82
Samuel Lowe <samlowe106@gmail.com>
2025-01-03 21:47:52 -0500

    Removed determine_name

--- core/__init__.py
+++ core/__init__.py
@@ -1,2 +1,2 @@
-from .core import determine_name, get_extension, retitle
+from .core import get_extension, retitle
 from .reddit import SortOption, SubmissionWrapper, from_saved, from_subreddit, sign_in

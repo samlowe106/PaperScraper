8d0cbfc8c9f1eafcbe0de94fd7a4263aed4a01ab
Samuel Lowe <samlowe106@gmail.com>
2026-05-26 00:30:41 -0400

    Moved retitle from SubmissionWrapper into file_manager

--- src/core/file_manager.py
+++ src/core/file_manager.py
@@ -1,7 +1,32 @@
 import datetime
 import os
+import re
 
 type DownloadsExtensions = list[tuple[bytes, str]]
+
+INVALID_WINDOWS_CHARS = r'<>:"/\|?*'
+
+
+def retitle(s: str) -> str:
+    """
+    Creates a valid filename based on the given title string without duplicate whitespace or
+    leading/trailing punctuation
+    :param s: the string that the created filename should be based on
+    :return: a valid, aesthetically pleasing filename
+    """
+    # remove characters that windows doesn't allow in filenames
+    s = s.replace('"', "'")
+    for char in INVALID_WINDOWS_CHARS:
+        s = s.replace(char, "")
+
+    # remove any duplicate whitespace
+    s = " ".join(s.split())
+
+    # trim punctuation ("." and ",") from the start and end of the string
+    r = re.compile(r"^([\.,]*)([^\.,]*)([\.,]*)\Z")
+    s = r.match(s).group(2)
+
+    return s[:250]
 
 
 class UniqueDirectoryFileManager:

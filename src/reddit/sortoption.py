0bfc0f5f583de57e066aa58a626a9c2e474f2c4f
Samuel Lowe <samlowe106@gmail.com>
2026-06-20 02:33:03 -0400

    Made this stuff partials so they aren't descriptors, and can actually be used

--- src/reddit/sortoption.py
+++ src/reddit/sortoption.py
@@ -1,35 +1,28 @@
-from enum import Enum
-
-from asyncpraw.models.subreddits import Subreddit
+from enum import Enum, member
+from functools import partial
 
 
 class SortOption(Enum):
     """
-    Represents the ways a subreddit's submissions can be sorted
+    Represents the ways a subreddit's submissions can be sorted.
+
+    Each member's value is a ``functools.partial`` wrapping a small lambda that
+    delegates to the subreddit's listing method, so ``SortOption.HOT(subreddit)``
+    is equivalent to ``subreddit.hot()``. ``enum.member()`` is required because a
+    bare ``partial`` is treated as a method descriptor (not a member) on modern
+    Python.
     """
 
-    def TOP_ALL(subreddit, **kwargs):
-        return Subreddit.top(self=subreddit, time_filter="all", **kwargs)
-
-    def TOP_DAY(subreddit, **kwargs):
-        return Subreddit.top(self=subreddit, time_filter="day", **kwargs)
-
-    def TOP_HOUR(subreddit, **kwargs):
-        return Subreddit.top(self=subreddit, time_filter="hour", **kwargs)
-
-    def TOP_WEEK(subreddit, **kwargs):
-        return Subreddit.top(self=subreddit, time_filter="week", **kwargs)
-
-    def TOP_MONTH(subreddit, **kwargs):
-        return Subreddit.top(self=subreddit, time_filter="month", **kwargs)
-
-    def TOP_YEAR(subreddit, **kwargs):
-        return Subreddit.top(self=subreddit, time_filter="year", **kwargs)
-
-    NEW = Subreddit.new
-    HOT = Subreddit.hot
-    CONTROVERSIAL = Subreddit.controversial
-    GILDED = Subreddit.gilded  # probably deprecated?
+    NEW = member(partial(lambda s, **kw: s.new(**kw)))
+    HOT = member(partial(lambda s, **kw: s.hot(**kw)))
+    CONTROVERSIAL = member(partial(lambda s, **kw: s.controversial(**kw)))
+    GILDED = member(partial(lambda s, **kw: s.gilded(**kw)))  # probably deprecated?
+    TOP_ALL = member(partial(lambda s, **kw: s.top(time_filter="all", **kw)))
+    TOP_DAY = member(partial(lambda s, **kw: s.top(time_filter="day", **kw)))
+    TOP_HOUR = member(partial(lambda s, **kw: s.top(time_filter="hour", **kw)))
+    TOP_WEEK = member(partial(lambda s, **kw: s.top(time_filter="week", **kw)))
+    TOP_MONTH = member(partial(lambda s, **kw: s.top(time_filter="month", **kw)))
+    TOP_YEAR = member(partial(lambda s, **kw: s.top(time_filter="year", **kw)))
 
     def __call__(self, *args, **kwargs):
-        self.value(*args, **kwargs)
+        return self.value(*args, **kwargs)
